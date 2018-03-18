edda9cff64b5c0fefa330399635579f0aa3913c9
asrp <asrp@email.com>
2018-03-18 10:26:52 -0400

    Add setup.py and reorganize directory structure.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='persistent_doc',
+      version='0.5',
+      description='An XML-like document with spreadsheet formulas for values and underlying persistent data structures',
+      url='https://github.com/asrp/persistent_doc',
+      author='asrp',
+      author_email='asrp@email.com',
+      packages=['persistent_doc'],
+      keywords='persistent tree document formula')
