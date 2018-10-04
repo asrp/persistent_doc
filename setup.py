4c00fd6c20c01abb762b26d62cb1e34bce28a349
asrp <asrp@email.com>
2018-10-04 10:08:29 -0400

    Update setup.py with markdown version of readme and find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(name='persistent_doc',
       version='0.5',
       description='An XML-like document with spreadsheet formulas for values and underlying persistent data structures',
+      long_description=open("readme.md").read(),
+      long_description_content_type="text/markdown",
       url='https://github.com/asrp/persistent_doc',
       author='asrp',
       author_email='asrp@email.com',
-      packages=['persistent_doc'],
+      packages=find_packages(),
       keywords='persistent tree document formula')
